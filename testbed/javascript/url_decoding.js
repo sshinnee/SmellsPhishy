0243731a5a28a15c2ba6e10c3a2ec0d49f6aba54
awesomebaby <awesomebaby@awesomebabys-MacBook-Air.local>
2018-10-28 11:27:17 +0800

    update - testbed now reads page title to get semantic intent of page -> next step: google search page title to get top url hit
    also added test phishing sites for testing

--- testbed/javascript/url_decoding.js
+++ testbed/javascript/url_decoding.js
@@ -1,3 +1,5 @@
+var dictionary = readDictionaryFile("wordlist.txt");
+
 function displaySimilarURLs() {
 	var input_urls_raw = document.getElementById('user_input').value;
 	var input_urls = input_urls_raw.split('\n');
@@ -11,10 +13,103 @@ function displaySimilarURLs() {
 	document.getElementById('similar_urls').innerHTML = "List of Similar URLs:<br><br> " + output_urls_formatted;
 }
 function identifySimilarURL(url) {
-	return url + " hello";
+	var newurl = readPageTitle(url);
+	console.log("our new similar url: " + newurl.toString());
+	// if (containsUnusualCharacters(url)) {
+	// 	newurl = removeHomographs(url);
+	// }
+	//read page title
+	//tokenize the url
+	newurl = makeGoogleSearch(newurl);
+	var tokens = url.split(".")
+	for (var i=0; i<tokens.length; i++) {
+		if (!inDictionary(tokens[i], dictionary)) {
+			//make a google search for any brands.companies
+			continue;
+		}
+		//token[i] 
+	}
+	return newurl;
+}
+
+function makeGoogleSearch(phrase) {
+	console.log("trying to read this url now: " + url.toString());
+	var title = "";
+	var rawFile = new XMLHttpRequest();
+	rawFile.open("GET", "http://www.google.com/search?q=" + phrase.toString(), false);
+	rawFile.onreadystatechange = function () {
+		if (rawFile.readyState === 4) {
+			if (rawFile.status === 200 || rawFile.status == 0) {
+				text = rawFile.responseText;
+				console.log("woot we are in google page");
+			}
+		}
+	}
+	rawFile.send();
+	responseHeaders = rawFile.getAllResponseHeaders(); 
+	////*[@id="rso"]/div/div/div[1]/div/div/div[1]/a
+	////*[@id="rso"]/div/div/div[1]/div/div/div[1]/a
+	console.log("response headers for this url: " + responseHeaders.toString());
+	title = rawFile.responseText.toString().split("<title>")[1].split("</title>")[0];
+	console.log("this is your page: \n" + title.toString());
+	console.log("end of page");
+	return title;//responseHeaders;//title;
+}
+
+function readDictionaryFile(filename) {
+	var text = "";
+	var rawFile = new XMLHttpRequest();
+	rawFile.open("GET", filename, false);
+	rawFile.onreadystatechange = function () {
+		if (rawFile.readyState === 4) {
+			if (rawFile.status === 200 || rawFile.status == 0) {
+				text = rawFile.responseText;
+				console.log("we actually got in here")
+			}
+		}
+	}
+	//rawFile.send();
+	text = rawFile.responseText;
+	console.log("this is your dictionary: \n" + text.toString());
+	console.log("end of dictionary");
+	return text;
+}
+
+function inDictionary(token, dictionary) {
+	var in_dictionary = dictionary.includes(token);
+	console.log(token.toString() + " is in dictionary: " + in_dictionary);
+	return in_dictionary;
+}
+
+function readPageTitle(url) {
+	console.log("trying to read this url now: " + url.toString());
+	var title = "";
+	var rawFile = new XMLHttpRequest();
+	rawFile.open("GET", url, false);
+	rawFile.onreadystatechange = function () {
+		if (rawFile.readyState === 4) {
+			if (rawFile.status === 200 || rawFile.status == 0) {
+				text = rawFile.responseText;
+				console.log("woot we are in page");
+			}
+		}
+	}
+	rawFile.send();
+	responseHeaders = rawFile.getAllResponseHeaders();
+	console.log("response headers for this url: " + responseHeaders.toString());
+	title = rawFile.responseText.toString().split("<title>")[1].split("</title>")[0];
+	console.log("this is your page: \n" + title.toString());
+	console.log("end of page");
+	return title;//responseHeaders;//title;
 }
 
 function evaluateURL(url) {
+	//[immediate flag]does the url have multiple '.' after '.com'?
+	//[suspicious flag]suspicious domains [.VE, .CC, .TK, .PW, .GA, .CF, .GQ, .ML, .BD, .KE, .CENTER, .NG,
+	//.PK, .RU] <- this list was gotten from https://docs.apwg.org/reports/APWG_Global_Phishing_Report_2015-2016.pdf
+	//[slightly suspicious flag] [.TOP, .XYZ, .ONLINE, .WIN, .SITE, .LINK, .CLUB, .WEBSITE
+	//.CENTER, .TRADE] 
+
 	return;
 }
 
@@ -29,7 +124,8 @@ function sanitizeURL(url) {
 }
 
 function containsUnusualCharacters(url) {
-	return false;
+	console.log(url);
+	return url.includes("xn--");
 }
 
 function removeHomographs(url) {
