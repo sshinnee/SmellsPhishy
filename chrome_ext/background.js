b0fc70e4b0cc009054bf4f22aded88fb07e0e1ca
smarteatsmarties <smarteatsmarties@hotmail.com>
2018-11-23 03:21:34 +0800

    Updated background.js
    Added Samantha URL code.
    Added checks for URL and similarURL to determine if further checks required.

--- chrome_ext/background.js
+++ chrome_ext/background.js
@@ -7,6 +7,7 @@
 // Global Variables
 var toDetectPunyCode = true;
 var toDetectRedirectCode = true;
+var dictionary = readDictionaryFile("wordlist.txt");
 
 // OnChanged event
 // Update the button values when users enable/disable them
@@ -441,22 +442,181 @@ function checkWebsiteTraffic(reqUrl, posOrigUrl, apiKey)
 // This serves as the main controller function that calls the various phishing checks
 function checkPhishing(url, details) {
 	/* likely need to return an array containing [immediate_failure, probability score] for each check */
-	//checkUrl(url);
-	//checkDomain(url);
-	//checkPageStats(url, origUrl);
-	//checkContent(url);
 	
-	var userAction = confirm("Continue with redirect?\n\n"
-							 + "Status code: " + details.statusCode + "\n"
-							 + "Original URL: " + details.url + "\n"
-							 + "Initiator: " + details.initiator + "\n"
-							 + "Redirected URL: " + url + "\n"
-							 + "WhoIs: " + checkDomain(url));
-		  
-	if (userAction != true) 
+	var similarURL = identifySimilarURL(url);
+	if (url === similarURL)
 	{
-		var blockingResponse = {};
-		blockingResponse.cancel = true;
-		return blockingResponse;
+		// Identical URL. No phishing check required.
+		console.log("Identical URL. No phishing check.");
 	}
+	else
+	{
+		//checkDomain(url);
+		//checkPageStats(url, similarURL);
+		//checkContent(url);
+		
+		var userAction = confirm("Continue with redirect?\n\n"
+								 + "Status code: " + details.statusCode + "\n"
+								 + "Original URL: " + details.url + "\n"
+								 + "Initiator: " + details.initiator + "\n"
+								 + "Redirected URL: " + url + "\n"
+								 + "WhoIs: " + checkDomain(url));
+			  
+		if (userAction != true) 
+		{
+			var blockingResponse = {};
+			blockingResponse.cancel = true;
+			return blockingResponse;
+		}
+	}
+}
+
+// Samantha Functions
+function identifySimilarURL(url) {
+	//read page title
+	var page_title = readPageTitle(url);
+	console.log("page title is: " + page_title.toString());
+	// if (containsUnusualCharacters(url)) {
+	// 	newurl = removeHomographs(url);
+	// }
+	var newurl = makeGoogleSearch(page_title);
+
+	//tokenize the url
+	var tokens = url.split(".")
+	for (var i=0; i<tokens.length; i++) {
+		if (!inDictionary(tokens[i], dictionary)) {
+			//make a google search for any brands.companies
+			continue;
+		}
+		//token[i] 
+	}
+	return newurl;
+}
+
+function getElementByXpath(path) {
+  return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+}
+
+function getTopHit(response) {
+	var el = document.createElement( 'html' );
+	el.innerHTML = response;
+	console.log("inside top hit function: ");
+	var newurl = el.querySelectorAll('cite.iUh30')[0].outerHTML.split(">")[1].split("<")[0];
+	console.log("new url from top hit function is: " + newurl);
+	return newurl;
+}
+
+function httpGet(theUrl)
+{
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
+    xmlHttp.send( null );
+    console.log("returning response from httpGet: " + xmlHttp.responseText);
+    return xmlHttp.responseText;
+}
+
+function makeHTTPRequestASync(url) {
+    var xmlHttp = new XMLHttpRequest();
+
+    xmlHttp.onreadystatechange = function() { 
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+        	console.log("we are receigin http response from: " + url);
+            return xmlHttp.responseText;
+    }
+    xmlHttp.open("GET", url, true); // true for asynchronous 
+    xmlHttp.setRequestHeader('Access-Control-Allow-Headers', 'Origin, Content-Type, X-Auth-Token');
+    xmlHttp.setRequestHeader('Access-Control-Allow-Methods', '*');
+    xmlHttp.setRequestHeader('Access-Control-Allow-Origin', '*');
+    xmlHttp.send(null);
+    console.log("random blah: " + url);
+    //return xmlHttp.responseText;
+}
+function makeGoogleSearch(phrase) {
+	var query_phrase = phrase.replace(/\s+/g, "+");
+	console.log("trying to read make google request for this: " + phrase.toString());
+	var query = "http://www.google.com/search?q=" + query_phrase.toString();
+	var google_hit_response = httpGet(query);//makeHTTPRequestASync(query);
+	console.log("google search response is: " + google_hit_response);
+
+	var newurl = getTopHit(google_hit_response);
+	console.log("new url woot woot change applied: " + newurl.toString());
+
+	return newurl;
+}
+
+function readDictionaryFile(filename) {
+	var text = "";
+	var rawFile = new XMLHttpRequest();
+	rawFile.open("GET", filename, false);
+	rawFile.onreadystatechange = function () {
+		if (rawFile.readyState === 4) {
+			if (rawFile.status === 200 || rawFile.status == 0) {
+				text = rawFile.responseText;
+				console.log("we actually got in here");
+			}
+		}
+	}
+	//rawFile.send();
+	text = rawFile.responseText;
+	console.log("this is your dictionary: \n" + text.toString());
+	console.log("end of dictionary");
+	return text;
+}
+
+function inDictionary(token, dictionary) {
+	var in_dictionary = dictionary.includes(token);
+	console.log(token.toString() + " is in dictionary: " + in_dictionary);
+	return in_dictionary;
+}
+
+function readPageTitle(url) {
+	console.log("trying to read this url now: " + url.toString());
+	var title = "";
+	var text = "";
+	var rawFile = new XMLHttpRequest();
+	rawFile.open("GET", url, false);
+	rawFile.onreadystatechange = function () {
+		if (rawFile.readyState === 4) {
+			if (rawFile.status === 200 || rawFile.status == 0) {
+				text = rawFile.responseText;
+				console.log("woot we are in page");
+			}
+		}
+	}
+	rawFile.send(null);
+	var responseHeaders = rawFile.getAllResponseHeaders();
+	console.log("response headers for this url: " + responseHeaders.toString());
+	title = rawFile.responseText.toString().split("<title>")[1].split("</title>")[0];
+	console.log("this is your page: \n" + title.toString());
+	console.log("end of page");
+	return title;//responseHeaders;//title;
+}
+
+function evaluateURL(url) {
+	//[immediate flag]does the url have multiple '.' after '.com'?
+	//[suspicious flag]suspicious domains [.VE, .CC, .TK, .PW, .GA, .CF, .GQ, .ML, .BD, .KE, .CENTER, .NG,
+	//.PK, .RU] <- this list was gotten from https://docs.apwg.org/reports/APWG_Global_Phishing_Report_2015-2016.pdf
+	//[slightly suspicious flag] [.TOP, .XYZ, .ONLINE, .WIN, .SITE, .LINK, .CLUB, .WEBSITE
+	//.CENTER, .TRADE] 
+
+	return;
+}
+
+function sanitizeURL(url) {
+	var flags = [];
+	if (containsUnusualCharacters(url)) {
+		flags.push(['uc']);
+		return;
+	} else {
+		return;
+	}
+}
+
+function containsUnusualCharacters(url) {
+	console.log(url);
+	return url.includes("xn--");
+}
+
+function removeHomographs(url) {
+	return;
 }
