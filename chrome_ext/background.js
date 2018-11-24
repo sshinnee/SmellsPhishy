6208bd4437186ac680fe5e989160518d072eb180
LPK2018 <45236040+LPK2018@users.noreply.github.com>
2018-11-24 10:26:28 +0800

    Update background.js
    Updated my portion

--- chrome_ext/background.js
+++ chrome_ext/background.js
@@ -191,7 +191,7 @@ function checkPhishing(url, details) {
 	{
 		//checkDomain(url);
 		//checkPageStats(url, similarURL);
-		//checkContent(url);
+		checkContent(url);
 		
 		//var userAction = confirm("Continue with redirect?\n\n"
 		//						 + "Status code: " + details.statusCode + "\n"
@@ -207,6 +207,32 @@ function checkPhishing(url, details) {
 			return blockingResponse;
 		}
 	}
+}
+
+//PK
+function checkContent(url) {
+
+	var strFormArray = phishingText.split("<form");
+	var indForm;
+	var likelyPhishing = false;
+	var suspiciousWords = ["password", "login", "address", "userid", "username"];
+
+	if (strFormArray.length > 0) {
+
+		for (var i = 1; i < strFormArray.length; i++) {
+			indForm = strFormArray[i].substr(0, strFormArray[i].indexOf("</form>"));
+			//console.log("PK: " + indForm);
+
+			for (var j = 0; j < suspiciousWords.length; j++) {
+				if (indForm.toLowerCase().includes(suspiciousWords[j])) {
+					likelyPhishing = true;
+				}
+			}
+		}
+	}
+
+	console.log("PK: " + likelyPhishing);
+	return likelyPhishing;
 }
 
 // Samantha Functions
