2fe7718dd4b977074c43f0896c94c453b3e0c6db
LPK2018 <45236040+LPK2018@users.noreply.github.com>
2018-11-25 17:40:58 +0800

    Update background.js
    Moved contentchecks code to js

--- chrome_ext/background.js
+++ chrome_ext/background.js
@@ -192,32 +192,6 @@ function checkPhishing(url, details) {
 	}
 }
 
-//PK
-function checkContent(url) {
-
-	var strFormArray = phishingText.split("<form");
-	var indForm;
-	var likelyPhishing = false;
-	var suspiciousWords = ["password", "login", "address", "userid", "username"];
-
-	if (strFormArray.length > 0) {
-
-		for (var i = 1; i < strFormArray.length; i++) {
-			indForm = strFormArray[i].substr(0, strFormArray[i].indexOf("</form>"));
-			//console.log("PK: " + indForm);
-
-			for (var j = 0; j < suspiciousWords.length; j++) {
-				if (indForm.toLowerCase().includes(suspiciousWords[j])) {
-					likelyPhishing = true;
-				}
-			}
-		}
-	}
-
-	console.log("PK: " + likelyPhishing);
-	return likelyPhishing;
-}
-
 // Samantha Functions
 function identifySimilarURL(url) {
 	//read page title
